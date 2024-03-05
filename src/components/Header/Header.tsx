3b5dbb348e1390e63bf0114472c49fe608d6db2b
Mehemmed Qardasov <mehemmed.qardasov2003@gmail.com>
2024-03-05 18:37:11 +0400

    .

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -13,8 +13,8 @@ const Header: React.FC = () => {
                     <h1 className='text-center'>La Ilaha İllallah</h1>
                 </Col>
                 <Col lg={{span: 6, offset:1}} md={{span: 6, offset:1}} sm={12} className='col2'>
-                    <p style={{fontSize: "22px", color: "#36454F"}}>Our Almighty Lord did not create us in vain</p>
-                    <b style={{fontSize: "20px"}}>Almighty Allah says:</b>
+                    <p style={{fontSize: "22px", color: "#36454F"}}>Our Almighty Lord did not create us in absurdness, in vain.</p>
+                    <b style={{fontSize: "20px"}}>Almighty Allah says this:</b>
                     <p style={{fontSize: "22px", textAlign: "center", marginTop: "30px", color: "red"}}>أَفَحَسِبْتُمْ أَنَّمَا خَلَقْنَـٰكُمْ عَبَثًۭا وَأَنَّكُمْ إِلَيْنَا لَا تُرْجَعُونَ
                     <br/>
                     <br/>
@@ -25,8 +25,8 @@ const Header: React.FC = () => {
 
                     <hr/>
                     
-                    <p style={{fontSize: "22px", color: "#36454F", marginTop: "80px"}}>Our Almighty Lord did not create us in vain</p>
-                    <b style={{fontSize: "20px"}}>Almighty Allah says:</b>
+                    <p style={{fontSize: "22px", color: "#36454F", marginTop: "80px"}}>He has not left us as wandering beings who are aimless and irresponsible for orders and prohibitions.</p>
+                    <b style={{fontSize: "20px"}}>Almighty Allah says this:</b>
                     <p style={{fontSize: "22px", textAlign: "center", marginTop: "30px", color: "red"}}>أَيَحْسَبُ ٱلْإِنسَـٰنُ أَن يُتْرَكَ سُدًى 
                     <br/>
                     <br/>
@@ -38,8 +38,8 @@ const Header: React.FC = () => {
                     <hr/>
                     
 
-                    <p style={{fontSize: "22px", color: "#36454F", marginTop: "80px"}}>Our Almighty Lord did not create us in vain</p>
-                    <b style={{fontSize: "20px"}}>Almighty Allah says:</b>
+                    <p style={{fontSize: "22px", color: "#36454F", marginTop: "80px"}}>He created us not for anything else, but only so that we can worship him.</p>
+                    <b style={{fontSize: "20px"}}>Almighty Allah says this:</b>
                     <p style={{fontSize: "22px", textAlign: "center", marginTop: "30px", color: "red"}}>وَمَا خَلَقْتُ ٱلْجِنَّ وَٱلْإِنسَ إِلَّا لِيَعْبُدُونِ
                     <br/>
                     <br/>
@@ -51,7 +51,7 @@ const Header: React.FC = () => {
                     <hr/>
 
 
-                    <p style={{fontSize: "22px", color: "#36454F", marginTop: "80px"}}>Our Almighty Lord did not create us in vain</p>
+                    <p style={{fontSize: "22px", color: "#36454F", marginTop: "80px"}}> He has sent a Messenger to us who recites His verses, teaches us the Book that purifies us and teaches us Wisdom, about what we did not know.</p>
                     <b style={{fontSize: "20px"}}>Almighty Allah says:</b>
                     <p style={{fontSize: "22px", textAlign: "center", marginTop: "30px", color: "red"}}>لَقَدْ مَنَّ ٱللَّهُ عَلَى ٱلْمُؤْمِنِينَ إِذْ بَعَثَ فِيهِمْ رَسُولًۭا مِّنْ أَنفُسِهِمْ يَتْلُوا۟ عَلَيْهِمْ ءَايَـٰتِهِۦ وَيُزَكِّيهِمْ وَيُعَلِّمُهُمُ ٱلْكِتَـٰبَ وَٱلْحِكْمَةَ وَإِن كَانُوا۟ مِن قَبْلُ لَفِى ضَلَـٰلٍۢ مُّبِينٍ
                     <br/>
@@ -64,8 +64,8 @@ const Header: React.FC = () => {
                     <hr/>
                     
                     
-                    <p style={{fontSize: "22px", color: "#36454F", marginTop: "80px"}}>Our Almighty Lord did not create us in vain</p>
-                    <b style={{fontSize: "20px"}}>Almighty Allah says:</b>
+                    <p style={{fontSize: "22px", color: "#36454F", marginTop: "80px"}}>The Messenger sent by our Lord to us and to all mankind until the day of Resurrection is Muhammad, the son of Abdullah, sallallahu alayhiwa sallam.</p>
+                    <b style={{fontSize: "20px"}}>Almighty Allah says this:</b>
                     <p style={{fontSize: "22px", textAlign: "center", marginTop: "30px", color: "red"}}>مُّحَمَّدٌۭ رَّسُولُ ٱللَّهِ ۚ
                     <br/>
                     <br/>
@@ -77,8 +77,8 @@ const Header: React.FC = () => {
                     <hr/>
 
 
-                    <p style={{fontSize: "22px", color: "#36454F", marginTop: "80px"}}>Our Almighty Lord did not create us in vain</p>
-                    <b style={{fontSize: "20px"}}>Almighty Allah says:</b>
+                    <p style={{fontSize: "22px", color: "#36454F", marginTop: "80px"}}>No Messenger will come after Him, nor a Prophet.</p>
+                    <b style={{fontSize: "20px"}}>Almighty Allah says this:</b>
                     <p style={{fontSize: "22px", textAlign: "center", marginTop: "30px", color: "red"}}>مَّا كَانَ مُحَمَّدٌ أَبَآ أَحَدٍۢ مِّن رِّجَالِكُمْ وَلَـٰكِن رَّسُولَ ٱللَّهِ وَخَاتَمَ ٱلنَّبِيِّـۧنَ ۗ وَكَانَ ٱللَّهُ بِكُلِّ شَىْءٍ عَلِيمًۭا
                     <br/>
                     <br/>
@@ -91,7 +91,7 @@ const Header: React.FC = () => {
 
 
                     <p style={{fontSize: "22px", color: "#36454F", marginTop: "80px"}}>Our Almighty Lord did not create us in vain</p>
-                    <b style={{fontSize: "20px"}}>Almighty Allah says:</b>
+                    <b style={{fontSize: "20px"}}>Almighty Allah says this:</b>
                     <p style={{fontSize: "22px", textAlign: "center", marginTop: "30px", color: "red"}}>قُلْ إِن كُنتُمْ تُحِبُّونَ ٱللَّهَ فَٱتَّبِعُونِى يُحْبِبْكُمُ ٱللَّهُ وَيَغْفِرْ لَكُمْ ذُنُوبَكُمْ ۗ وَٱللَّهُ غَفُورٌۭ رَّحِيمٌۭ <br/>  قُلْ أَطِيعُوا۟ ٱللَّهَ وَٱلرَّسُولَ ۖ فَإِن تَوَلَّوْا۟ فَإِنَّ ٱللَّهَ لَا يُحِبُّ ٱلْكَـٰفِرِينَ
                     <br/>
                     <br/>
@@ -105,8 +105,8 @@ const Header: React.FC = () => {
                     <hr/>
 
 
-                    <p style={{fontSize: "22px", color: "#36454F", marginTop: "80px"}}>Our Almighty Lord did not create us in vain</p>
-                    <b style={{fontSize: "20px"}}>Almighty Allah says:</b>
+                    <p style={{fontSize: "22px", color: "#36454F", marginTop: "80px"}}>Whoever obeys the Messenger of Allah, Muhammad, may Allah’s peace and blessings be upon him, will achieve eternal salvation. Whoever disobeys Him and disobeys Him will perish forever.</p>
+                    <b style={{fontSize: "20px"}}>Almighty Allah says this:</b>
                     <p style={{fontSize: "22px", textAlign: "center", marginTop: "30px", color: "red"}}>وَلَقَدْ بَعَثْنَا فِى كُلِّ أُمَّةٍۢ رَّسُولًا أَنِ ٱعْبُدُوا۟ ٱللَّهَ وَٱجْتَنِبُوا۟ ٱلطَّـٰغُوتَ ۖ
                     <br/>
                     <br/>
