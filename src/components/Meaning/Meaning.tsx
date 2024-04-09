d1991cf298761138ba94dadae147a84655e6c4b6
Mehemmed Qardasov <mehemmed.qardasov2003@gmail.com>
2024-04-10 00:02:04 +0400

    .

--- src/components/Meaning/Meaning.tsx
+++ src/components/Meaning/Meaning.tsx
@@ -13,11 +13,109 @@ const Meaning: React.FC = () => {
                     <h2 className='text-center'>MEANING OF LA ILAHE ILLALLAH</h2>
                 </Col>
                 <Col lg={{span: 6, offset:1}} md={{span: 6, offset:1}} sm={12} className='col2'>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit explicabo ea aliquid omnis, quisquam placeat dolorem expedita fugit, iste odit sunt optio aut voluptatem ad vel alias voluptate quia deserunt!
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit explicabo ea aliquid omnis, quisquam placeat dolorem expedita fugit, iste odit sunt optio aut voluptatem ad vel alias voluptate quia deserunt!
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit explicabo ea aliquid omnis, quisquam placeat dolorem expedita fugit, iste odit sunt optio aut voluptatem ad vel alias voluptate quia deserunt!
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit explicabo ea aliquid omnis, quisquam placeat dolorem expedita fugit, iste odit sunt optio aut voluptatem ad vel alias voluptate quia deserunt!
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit explicabo ea aliquid omnis, quisquam placeat dolorem expedita fugit, iste odit sunt optio aut voluptatem ad vel alias voluptate quia deserunt!
+                    <p style={{fontSize: "22px"}}>The english equivalent of the expression <b><i>La ilahe illallah</i></b>, according to its word order, is as follows: </p>
+                    <b>“There is no true god except Allah!” </b>
+                    <p style={{fontSize: "22px"}}>Understanding the meaning expressed in this sentence depends on knowing correctly what the word <i><b>"ilah"</b></i> means</p>
+
+                    <p>The word <b><u>"Ilah"</u></b>, according to the consensus of all Arab language scholars, means <b><i>ma'bud</i></b> that is, something that is worshiped or worshiped.</p>
+                    <br/>
+                    
+                    <p>After replacing the word <b><i>Ilah</i></b> in tabove expression with the word <b><i>ma'bud</i></b>, which is the correct meaning of this word, said, <b><i>"There is no true god, except Allah!"</i></b> Understanding what the meaning expressed the form means depends on knowing correctly what the meaning of <b><i>worship</i></b> is, which emphasized in the word <b><i>ma'bud</i></b>, that is, the one who is worshiped.</p>
+                    <br/>
+                    
+                    <p><b><u>Worship</u></b>, that is, worship with complete love, fear and hope; It shows humiliation, being needy, helplespoor and despised, and submitting with complete devotion from the heart.</p>
+                    <br/>
+
+                    <p>All actions that Allah loves and approves, verbally or physically, inwardly or outwardly, are acts worship. Begging, praying, seeking help, taking refuge, prostrating, making a vow, asacrificing a sacrifice are some of the most special examples of worship.</p>
+                    <br/>
+
+                    <p>So, there is <b><i>True Ilah, except Allah!</i></b> in eglish The word <b><i>la ilahe illallah</i></b>, which we translate as "one should be worshiped except Allah", that is, one should not beg or pray to anyoother than Him. It means that one should not pray to anyone other than Him, one shounot seek help from anyone other than Him, one should not take refuge in anyone other thHim, one should not prostrate oneself other than Him, one should not make vows, oshould not make sacrifices, and one should not worship or worship anyone other than Hwith any form of worship other than these.</p>
+                    <br/>
+
+                    <p>These two verses express this meaning of the word <b><i>la ilahe illallah</i></b> in a way that everyone can understand.</p>
+                    <br/>
+
+                    <p><b><u>First Verse</u></b></p>
+                    <b style={{fontSize: "20px"}}>Almighty Allah says this:</b>
+                    <p style={{fontSize: "22px", textAlign: "center", marginTop: "30px", color: "red", fontWeight: "bold"}}>قُلْ يَا أَهْلَ الْكِتَابِ تَعَالَوْا إِلَىٰ كَلِمَةٍ سَوَاءٍ بَيْنَنَا وَبَيْنَكُمْ أَلَّا نَعْبُدَ إِلَّا اللَّهَ وَلَا نُشْرِكَ بِهِ شَيْئًا وَلَا يَتَّخِذَ بَعْضُنَا بَعْضًا أَرْبَابًا مِنْ دُونِ اللَّهِ ۚ فَإِنْ تَوَلَّوْا فَقُولُوا اشْهَدُوا بِأَنَّا مُسْلِمُونَ</p>
+                    <br/>
+                    <p><i>"˹Remember, O  Prophet˺ when Abraham declared to his father and his people, “I am totally free of whatever ˹gods˺ you worship,except the One Who originated me, and He will surely guide me.” And he left this enduring declaration among his descendants, so they may ˹always˺ turn back ˹to Allah˺"</i></p>
+                    <p style={{fontSize: "20px", textAlign: "center", fontWeight: "bold"}}>(43/Az-Zukhruf, 26-28)</p>
+                    
+                    <p>The eternal word left by Ibrahim (peace be upon him) is the word <b>la ilahe illallah.</b></p>
+                    <br/>
+
+                    <p>He expressed the meaning of this word with the sentence <i>"I am free from what you worship, except for the one who created me."</i></p>
+                    <br/>
+
+                    <p>In the words of Ibrahim (peace be upon him)</p>
+                    <br/>
+                    
+                    <p style={{fontSize: "22px"}}>The statement <b>"I am free from what you worship"</b> means</p>
+                    <br/>
+                    
+                    <p style={{fontSize: "22px"}}>The word <b>“La ilahe”</b></p>
+                    <br/>
+
+                    <p style={{fontSize: "22px"}}>The expression <b>“illellezi, fetarani/except the one who created me”</b> is also</p>
+                    <br/>
+
+                    <p style={{fontSize: "22px"}}>It is the exact equivalent of the word <b>“illallah”</b>.</p>
+                    <br/>
+
+                    <p style={{fontSize: "22px"}}>So, the meaning of <b>“la ilahe illallah”</b> is to be free from everything that people worship except Allah, who created us.</p>
+                    <br/>
+
+                    <p style={{fontSize: "22px"}}>Abraham's statement that <b>"except the one who created me"</b> shows that one of the gods worshiped by those polytheists is Allah. In other words, polytheists are religious people who worship both Allah and others.</p>
+                    <br/>
+
+                    <p style={{fontSize: "22px"}}>Moreover, instead of saying <b>"except Allah"</b>, he says <b>"except the one who created me"</b>, in order to bind them by mentioning the fact that they accept that the Creator is Allah, as evidence against them. In other words, the polytheists believed that Allah was the only Creator, and when asked, they said so without hesitation.</p>
+                    <br/>
+
+                    <b style={{fontSize: "20px"}}>Almighty Allah says this:</b>
+                    <p style={{fontSize: "22px", textAlign: "center", marginTop: "30px", color: "red", fontWeight: "bold"}}>وَلَئِن سَأَلْتَهُم مَّنْ خَلَقَهُمْ لَيَقُولُنَّ ٱللَّهُ ۖ فَأَنَّىٰ يُؤْفَكُونَ</p>
+                    <br/>
+                    <p><i>"If you ask them ˹O Prophet˺ who created them, they will certainly say, “Allah!” How can they then be deluded ˹from the truth˺?"</i></p>
+                    <p style={{fontSize: "20px", textAlign: "center", fontWeight: "bold"}}>(43/Az-Zukhruf, 87)</p>
+
+                    <p><b><u>Second Verse</u></b></p>
+                    <br/>
+
+                    <b style={{fontSize: "20px"}}>Almighty Allah says this:</b>
+                    <p style={{fontSize: "22px", textAlign: "center", marginTop: "30px", color: "red", fontWeight: "bold"}}>قُلْ يَا أَهْلَ الْكِتَابِ تَعَالَوْا إِلَىٰ كَلِمَةٍ سَوَاءٍ بَيْنَنَا وَبَيْنَكُمْ أَلَّا نَعْبُدَ إِلَّا اللَّهَ وَلَا نُشْرِكَ بِهِ شَيْئًا وَلَا يَتَّخِذَ بَعْضُنَا بَعْضًا أَرْبَابًا مِنْ دُونِ اللَّهِ ۚ فَإِنْ تَوَلَّوْا فَقُولُوا اشْهَدُوا بِأَنَّا مُسْلِمُونَ</p>
+                    <br/>
+                    <p><i>"Say, ˹O Prophet,˺ “O People of the Book! Let us come to common terms: that we will worship none but Allah, associate none with Him, nor take one another as lords instead of Allah.” But if they turn away, then say, “Bear witness that we have submitted ˹to Allah alone˺.”"</i></p>
+                    <p style={{fontSize: "20px", textAlign: "center", fontWeight: "bold"}}>(3/Ali Imran, 64)</p>
+                    <br/>
+
+                    <p style={{fontSize: "22px"}}>The word that the Prophet sallallahu alayhi wa sallam invited them to is the word <b><i>la ilahe illallah</i></b>.</p>
+                    <br/>
+
+                    <p style={{fontSize: "22px"}}>The meaning of this word in the command of Almighty Allah is <b><i>"Let us not worship anyone, except Allah"</i></b> It is expressed in the sentence:</p>
+                    <br/>
+                    
+                    <p style={{fontSize: "22px"}}>The sentence <b><i>“la na'bude” “Let us not worship anyone”</i></b> in the verse</p>
+                    <br/>
+
+                    <p style={{fontSize: "22px"}}>The word <b><i>“La ilahe”</i></b></p>
+                    <br/>
+
+                    <p style={{fontSize: "22px"}}>The sentence <b><i>“illallah”</i></b> and <b><i>“other than Allah”</i></b> is also.</p>
+                    <br/>
+
+                    <p style={{fontSize: "22px"}}>It is the exact equivalent of the word <b><i>"illallah"</i></b>.</p>
+                    <br/>
+
+                    <p style={{fontSize: "22px"}}>The expression <b><i>"Let us not associate anything with Him"</i></b> is intended to reinforce this meaning.</p>
+                    <br/>
+
+                    <p style={{fontSize: "22px"}}>So, the meaning of <b><i>la ilahe illallah</i></b> is “Let us not worship anyone but Allah.” means</p>
+                    <br/>
+
+                    <p style={{fontSize: "22px"}}>As can be understood from all these, the word <b>la ilahe illallah</b> does not mean accepting the existence of  Allah or that He is the Creator, but it means uniting in worship the Almighty Allah, whose existence and sole creator is known to everyone, worshiping anyone other than Him and rejecting everything worshiped other than Him.</p>
+                    <br/>
+
+                    <p style={{fontSize: "22px", fontWeight: "bold"}}>This is what Tawhid is.</p>
                 </Col>
             </Row>
         </Container>
