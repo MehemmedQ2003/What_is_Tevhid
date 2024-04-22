927c063137bdceca528194378fbfdc87817be517
Mehemmed Qardasov <mehemmed.qardasov2003@gmail.com>
2024-04-22 16:37:31 +0400

    Update Essentials.tsx

--- src/components/Essentials/Essentials.tsx
+++ src/components/Essentials/Essentials.tsx
@@ -13,11 +13,169 @@ const Essentials: React.FC = () => {
                     <h2 className='text-center'>ESSENTIALS OF LA ILAHE ILLALLAH</h2>
                 </Col>
                 <Col lg={{span: 6, offset:1}} md={{span: 6, offset:1}} sm={12} className='col2'>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit explicabo ea aliquid omnis, quisquam placeat dolorem expedita fugit, iste odit sunt optio aut voluptatem ad vel alias voluptate quia deserunt!
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit explicabo ea aliquid omnis, quisquam placeat dolorem expedita fugit, iste odit sunt optio aut voluptatem ad vel alias voluptate quia deserunt!
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit explicabo ea aliquid omnis, quisquam placeat dolorem expedita fugit, iste odit sunt optio aut voluptatem ad vel alias voluptate quia deserunt!
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit explicabo ea aliquid omnis, quisquam placeat dolorem expedita fugit, iste odit sunt optio aut voluptatem ad vel alias voluptate quia deserunt!
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit explicabo ea aliquid omnis, quisquam placeat dolorem expedita fugit, iste odit sunt optio aut voluptatem ad vel alias voluptate quia deserunt!
+
+                    <p style={{fontSize: "22px"}}>The saying <b><i>"La ilahe illallah"</i></b> consists of two parts.</p>
+
+                    <p style={{fontSize: "22px"}}><b>First:</b> La ilahe.</p>
+
+                    <p style={{fontSize: "22px"}}><b>Second:</b> İllallah</p>
+
+                    <p style={{fontSize: "22px"}}>These are the pillars of <i>la ilahe illallah.</i></p>
+
+                    <p style={{fontSize: "22px"}}>The first one is called <b><i>Nefy.</i></b></p>
+
+                    <p style={{fontSize: "22px"}}>The second is called <b><i>Proof.</i></b></p>
+
+                    <p style={{fontSize: "22px"}}>Nefy means rejecting and proof means accepting. Nefy means denial and proof means confession.</p>
+
+                    <p style={{fontSize: "22px"}}>Four things are negated with the words <b><i>"La ilahe illallah".</i></b></p>
+
+                    <p style={{fontSize: "22px"}}>All four things are proven, that is, accepted and confessed</p>
+
+                    <p style={{fontSize: "22px"}}><b>The four things that are negated (denied) are asfollows:</b></p>
+
+                    <p style={{fontSize: "22px"}}>
+                        <b>1</b> - Gods <br />
+                        <b>2</b> - Taghuts <br />
+                        <b>3</b> - Fear and hope <br />
+                        <b>4</b> - Lords
+                    </p>
+
+                    <p style={{fontSize: "22px"}}><b>The four things that are Isbat (professed) also as follows:</b></p>
+
+                    <p style={{fontSize: "22px"}}>
+                        <b>1</b> - Intention <br />
+                        <b>2</b> - To bow and affection <br />
+                        <b>3</b> - Fear and hope <br />
+                        <b>4</b> - Acquittal from shirk and from the people of shirk
+                    </p>
+
+                    <p style={{fontSize: "22px"}}>The first thing that Almighty Allah has made obligatory on all His servants is to deny taghut and believe in Allah.</p>
+
+                    <hr />
+                    <br />
+                    
+                    <b style={{fontSize: "20px"}}>Almighty Allah says this:</b>
+                    <p style={{fontSize: "22px", textAlign: "center", marginTop: "30px", color: "red", fontWeight: "bold"}}>إِلَّاوَلَقَدْ بَعَثْنَا فِى كُلِّ أُمَّةٍۢ رَّسُولًا أَنِ ٱعْبُدُوا۟ ٱللَّهَ وَٱجْتَنِبُوا۟ ٱلطَّـٰغُوتَ ۖ فَمِنْهُم</p>
+                    <br/>
+                    <p style={{fontSize: "22px"}}><i>We surely sent a messenger to every community, saying, “Worship Allah and shun taghuts.”</i></p>
+                    <p style={{fontSize: "20px", textAlign: "center", fontWeight: "bold"}}>(16/An-Nahl, 36)</p>
+
+                    <br />
+                    <br />
+
+                    <p style={{fontSize: "22px"}}><b><i>"Beware of tağut" la ilaha illallah</i></b>, the first part of the word and the first ruknu nefy, <br /> <br />
+                    
+                    that is, <b><i>"La ilaha”</i></b>, <br /><br />
+                    
+                    In <b><i>“Worship Allah“</i></b>, it fully corresponds to the second part of the verse, <br /> <br />
+                    
+                    that is, <b><i>”Illallah".</i></b></p>
+
+                    <p style={{fontSize: "22px"}}>Therefore, <b><i>la ilaha illallah</i></b> means ”to avoid Tagut and worship Allah".</p>
+
+                    <p style={{fontSize: "22px"}}>A person cannot have faith in Allah unless he denies tagut.</p>
+
+                    <hr />
+                    <br />
+
+                    <b style={{fontSize: "20px"}}>Almighty Allah says this:</b>
+                    <p style={{fontSize: "20px", textAlign: "center", marginTop: "30px", color: "red", fontWeight: "bold"}}>فَمَن يَكْفُرْ بِٱلطَّـٰغُوتِ وَيُؤْمِنۢ بِٱللَّهِ فَقَدِ ٱسْتَمْسَكَ بِٱلْعُرْوَةِ ٱلْوُثْقَىٰ لَا ٱنفِصَامَ لَهَا ۗ </p>
+                    <br/>
+                    <p style={{fontSize: "22px", fontStyle: "italic"}}>So whoever renounces taghuts and believes in Allah has certainly grasped the firmest, unfailing hand-hold.</p>
+                    <p style={{fontSize: "20px", textAlign: "center", fontWeight: "bold"}}>(2/Al-Baqarah, 256)</p>
+
+                    <br />
+                    <br />
+
+                    <p style={{fontSize: "22px"}}><b><i>"Beware of tağut" la ilaha illallah</i></b>, the first part of the word and the first ruknu nefy, <br /> <br />
+
+                    that is, <b><i>"La ilaha”</i></b>, <br /><br />
+
+                    In <b><i>“Worship Allah“</i></b>, it fully corresponds to the second part of the verse, <br /> <br />
+
+                    that is, <b><i>”Illallah".</i></b></p>
+
+                    <br />
+
+                    <p style={{fontSize: "22px"}}>Almighty Allah has attributed holding on to a firm handle that will not break to these two things.</p>
+
+                    <p style={{fontSize: "22px"}}><b>The first is:</b> to deny the Tagut</p>
+
+                    <p style={{fontSize: "22px"}}><b>Secondly:</b> to believe in Allah</p>
+
+                    <p style={{fontSize: "22px"}}><b style={{fontSize: "30px"}}><u>Tagut:</u></b> It is everything that is worshipped besides Allah.</p>
+
+                    <p style={{fontSize: "22px"}}>Everyone who is worshiped and is satisfied with it, and who is pushed or obeyed by exceeding the legitimate limits, is a taghut..</p>
+
+                    <p style={{fontSize: "22px", fontWeight: "bolder"}}>There are many taghuts. The main ones are five.</p>
+
+                    <p style={{fontSize: "22px"}}>
+                        <b>1</b> - Satan <br />
+                        <b>2</b> - Calling to worship Him <br />
+                        <b>3</b> - Who is worshiped by him and is pleased with this <br />
+                        <b>4</b> - Who judges with judgments other than the judgments revealed by Allah <br />
+                        <b>5</b> - Who claims the knowledge of the unseen
+                    </p>
+
+                    <br />
+
+                    <p style={{fontSize: "22px", fontWeight: "bolder"}}>Denying tagut and avoiding it, that is, your first ruk, nephi, that is, La ilaha, is realized by fulfilling the following five things.</p>
+
+                    <p style={{fontSize: "22px"}}>
+                        <b>1</b> - Believing that it is superstitious to worship anyone other than Allah, <br />
+                        <b>2</b> - To abandon the worship of anyone other than Allah, <br />
+                        <b>3</b> - To hate to worship anyone other than Allah and those who worship anyone other than Him,  <br />
+                        <b>4</b> - To takfir worshipping someone other than Allah and to takfir those who worship someone other         than Him <br />
+                        <b>5</b> - To be hostile to things that are worshipped other than Allah and to those who worship them.
+                    </p>
+
+                    <br />
+
+                    <p style={{fontSize: "22px", fontWeight: "bolder"}}>Believing in Allah and worshipping Him, that is, the proof that is the second bow, that is, illallah, occurs by fulfilling these five things.</p>
+
+                    <p style={{fontSize: "22px"}}>
+                        <b>1</b> - To believe that Allah is the only true ilah, <br />
+                        <b>2</b> - To perform all worship to ALLAH <br />
+                        <b>3</b> - Removing every variety of worship from someone other than Allah <br />
+                        <b>4</b> - Loving the people of Ikhlas and Tawhid and taking them as guardians <br />
+                        <b>5</b> - To hate the people of polytheism and to make them enemies.
+                    </p>
+
+
+                    <hr />
+                    <br />
+
+                    <b style={{fontSize: "20px"}}>Almighty Allah says this:</b>
+                    <p style={{fontSize: "20px", textAlign: "center", marginTop: "30px", color: "red", fontWeight: "bold"}}>قَدْ كَانَتْ لَكُمْ أُسْوَةٌ حَسَنَةٌۭ فِىٓ إِبْرَٰهِيمَ وَٱلَّذِينَ مَعَهُۥٓ إِذْ قَالُوا۟ لِقَوْمِهِمْ إِنَّا بُرَءَٰٓؤُا۟ مِنكُمْ وَمِمَّا تَعْبُدُونَ مِن دُونِ ٱللَّهِ كَفَرْنَا بِكُمْ وَبَدَا بَيْنَنَا وَبَيْنَكُمُ ٱلْعَدَٰوَةُ وَٱلْبَغْضَآءُ أَبَدًا حَتَّىٰ تُؤْمِنُوا۟ بِٱللَّهِ وَحْدَهُۥٓ إِلَّا</p>
+                    <br/>
+                    <p style={{fontSize: "22px", fontStyle: "italic"}}>You already have an excellent example in Abraham and those with him, when they said to their people, “We totally dissociate ourselves from you and ˹shun˺ whatever ˹idols˺ you worship besides Allah. We reject you. The enmity and hatred that has arisen between us and you will last until you believe in Allah alone.” </p>
+                    <p style={{fontSize: "20px", textAlign: "center", fontWeight: "bold"}}>(60/Al-Mumtahanah, 4)</p>
+
+
+                    <hr />
+                    <br />
+
+                    <b style={{fontSize: "20px"}}>Almighty Allah says this:</b>
+                    <p style={{fontSize: "20px", textAlign: "center", marginTop: "30px", color: "red", fontWeight: "bold"}}>قَوَمَن يَرْغَبُ عَن مِّلَّةِ إِبْرَٰهِـۧمَ إِلَّا مَن سَفِهَ نَفْسَهُۥ ۚ وَلَقَدِ ٱصْطَفَيْنَـٰهُ فِى ٱلدُّنْيَا ۖ وَإِنَّهُۥ فِى ٱلْـَٔاخِرَةِ لَمِنَ ٱلصَّـٰلِحِينَ ١٣٠</p>
+                    <br/>
+                    <p style={{fontSize: "22px", fontStyle: "italic"}}>And who would reject the faith of Abraham except a fool! We certainly chose him in this life, and in the Hereafter he will surely be among the righteous.</p>
+                    <p style={{fontSize: "20px", textAlign: "center", fontWeight: "bold"}}>(2/Al-Baqarah, 130)</p>
+
+
+                    <hr />
+                    <br />
+
+                    <p style={{fontSize: "20px", textAlign: "center", fontWeight: "bold"}}>The Prophet (peace be upon him) also says:</p>
+                    <p style={{fontSize: "22px", textAlign: "center", marginTop: "30px", color: "red", fontWeight: "bold"}}>مَنْ قَالَ: لاَ إِلَهَ إِلاّ الله، وَكَفَرَ بِمَا يُعْبَدُ مِنْ دُونِ الله، حَرُمَ مَالُهُ وَدَمُهُ. وَحِسَابُهُ عَلَى الله</p>
+                    <p style={{fontSize: "22px"}}><i>I heard the Messenger of Allah (ﷺ) say: He who professed that there is no god but Allah and made a denial of everything which the people worship beside Allah, his property and blood became inviolable, an their affairs rest with Allah.</i></p>
+                    <p style={{fontSize: "20px", textAlign: "center", fontWeight: "bold"}}>(Muslim, 23)</p>
+
+
+                    <p style={{fontSize: "22px"}}>
+                        The Prophet sallallahu alayhi wa sallam did not consider it sufficient to say <b><i>la ilaha illallah</i></b> for property and life to be inviolable. Besides saying this, he did not consider it enough to know or even accept his mana, and moreover, not to worship anyone but Allah. Property and life are not inviolable unless we add to all this the rejection of things that are worshipped other than Allah. In other words, a person is not considered to have converted to Islam, become a Muslim.
+                    </p>
+
                 </Col>
             </Row>
         </Container>
