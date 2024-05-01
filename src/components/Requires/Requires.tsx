9481db0f6c0527c03c76818526beef6fea3ce3d1
Mehemmed Qardasov <mehemmed.qardasov2003@gmail.com>
2024-05-01 12:46:20 +0400

    first commit

--- src/components/Requires/Requires.tsx
+++ src/components/Requires/Requires.tsx
@@ -13,11 +13,165 @@ const Requires: React.FC = () => {
                     <h2 className='text-center'>WHAT LA ILAHE ILLALLAH REQUIRES</h2>
                 </Col>
                 <Col lg={{span: 6, offset:1}} md={{span: 6, offset:1}} sm={12} className='col2'>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit explicabo ea aliquid omnis, quisquam placeat dolorem expedita fugit, iste odit sunt optio aut voluptatem ad vel alias voluptate quia deserunt!
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit explicabo ea aliquid omnis, quisquam placeat dolorem expedita fugit, iste odit sunt optio aut voluptatem ad vel alias voluptate quia deserunt!
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit explicabo ea aliquid omnis, quisquam placeat dolorem expedita fugit, iste odit sunt optio aut voluptatem ad vel alias voluptate quia deserunt!
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit explicabo ea aliquid omnis, quisquam placeat dolorem expedita fugit, iste odit sunt optio aut voluptatem ad vel alias voluptate quia deserunt!
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit explicabo ea aliquid omnis, quisquam placeat dolorem expedita fugit, iste odit sunt optio aut voluptatem ad vel alias voluptate quia deserunt!
+                    
+
+                <p style={{fontSize: "22px", color: "gray"}}>The requirements of <b><i>La ilaha illallah</i></b> are the things that have a necessary connection between the belief in this word and the Decrees of Allah. That is, if this word has been believed, there are also these as necessary, and if there are not these, it means that this word has not been believed as necessary either. What should be done after to believe to <b><i>La ilaha illallah</i></b> will be mentioned in the next bet under the heading <b><i>“The Rights of La ilaha illallah”.</i></b></p>
+                    
+                    <p style={{fontSize: "22px"}}>The first of the things that <b><i>La ilaha illallah</i></b> requires: </p>
+                    
+                    <p style={{fontSize: "22px"}}><b style={{fontSize: "26px"}}>First: </b> To abandon the worship of everything that is worshipped besides Allah, that the ancestors worshipped</p>
+
+                    <br />
+                    <hr />
+                    
+                    <b style={{fontSize: "20px"}}>Almighty Allah says this:</b><p style={{fontSize: "22px", textAlign: "center", marginTop: "30px", color: "red", fontWeight: "bold"}}>قَالُواْ أَجِئْتَنَا لِنَعْبُدَ اللّهَ وَحْدَهُ وَنَذَرَ مَا كَانَ يَعْبُدُ آبَاؤُنَا</p>
+                    <br/>
+                    <p style={{fontSize: "22px"}}><i>They said, “Have you come to us so that we would worship Allah alone and abandon what our forefathers used to worship?</i></p>
+                    <p style={{fontSize: "20px", textAlign: "center", fontWeight: "bold"}}>(7/Al-A'raf, 70)</p>
+
+                    <br />
+                    <br />
+                    
+                    <p style={{fontSize: "22px"}}><b><i>"Let's put aside the things that our ancestors worshipped”</i></b></p>
+
+                    <p style={{fontSize: "22px"}}><b><i>"La ilaha,</i></b> that is, nephy,</p>
+
+                    <p style={{fontSize: "22px"}}>Say, <b><i>"Let us worship Allah alone.”</i></b></p>
+
+                    <p style={{fontSize: "22px"}}><b><i>"Illallah, </i></b> that is, it proven exactly</p>
+
+                    <p style={{fontSize: "22px"}}>Therefore, the requirement of <b><i>la ilaha illallah</i></b> is to put aside everything that the ancestors worshipped and worship Allah alone.</p>
+
+                    <br />
+                    
+                    <p style={{fontSize: "22px"}}><b style={{fontSize: "26px"}}>Second: </b>It is to worship Allah.</p>
+                    
+                    <p style={{fontSize: "22px"}}>In other words, it is not enough not to worship idols and graves. However, it is also necessary to worship Allah.</p>
+
+                    <br />
+                    <hr />
+                    
+                    <b style={{fontSize: "20px"}}>Almighty Allah says this:</b><p style={{fontSize: "22px", textAlign: "center", marginTop: "30px", color: "red", fontWeight: "bold"}}>وَالَّذِينَ اجْتَنَبُوا الطَّاغُوتَ أَن يَعْبُدُوهَا وَأَنَابُوا إِلَى اللَّهِ لَهُمُ الْبُشْرَى</p>
+                    <br/>
+                    <p style={{fontSize: "22px"}}><i>And those who shun the worship of false gods, turning to Allah ˹alone˺, will have good news. So give good news to My servants ˹O Prophet˺</i></p>
+                    <p style={{fontSize: "20px", textAlign: "center", fontWeight: "bold"}}>(39/Az-Zumar, 17)</p>
+
+                    <br />
+                    <br />
+                    
+                    <p style={{fontSize: "22px"}}><b><i>"To avoid worshipping the taghut”</i></b></p>
+
+                    <p style={{fontSize: "22px"}}><b><i>"La ilaha,</i></b> that is, nephy,</p>
+
+                    <p style={{fontSize: "22px"}}>Say, <b><i>"turning to Allah."</i></b></p>                    
+
+                    <p style={{fontSize: "22px"}}><b><i>"Illallah, </i></b> that is, it proven exactly</p>
+
+                    <p style={{fontSize: "22px"}}>The good news is for those who have achieved both of these at once.</p>
+                    
+                    <br />
+                    
+                    <p style={{fontSize: "22px"}}><b style={{fontSize: "26px"}}>Third: </b>It is to be hostile to things that are worshipped other than Allah and to those who worship them</p>
+
+                    <br />
+                    <hr />
+                    
+                    <b style={{fontSize: "20px"}}>Almighty Allah says this:</b><p style={{fontSize: "22px", textAlign: "center", marginTop: "30px", color: "red", fontWeight: "bold"}}>قَالَ أَفَرَأَيْتُم مَّا كُنتُمْ تَعْبُدُونَ* أَنتُمْ وَآبَاؤُكُمُ الْأَقْدَمُونَ* فَإِنَّهُمْ عَدُوٌّ لِّي إِلَّا رَبَّ الْعَالَمِينَ</p>
+                    <br/>
+                    <p style={{fontSize: "22px"}}><i>Abraham responded, “Have you ˹really˺ considered what you have been worshipping—you and your ancestors? They are ˹all˺ enemies to me, except the Lord of all worlds.</i></p>
+                    <p style={{fontSize: "20px", textAlign: "center", fontWeight: "bold"}}>(26/Ash-Shu'ara, 75-77)</p>
+
+
+                    <p style={{fontSize: "22px"}}><i>“Those things that you and your past ancestors worshipped, they are my enemies.”, Fully proven”La ilaha", that is, nephy"</i></p>
+
+                    <p style={{fontSize: "22px"}}><b><i>"Illa Rabbe'l-alemin / except the lord of the worlds"</i></b></p>
+
+                    <p style={{fontSize: "22px"}}>It fully proven <b><i>”Illallah“</i></b></p>
+
+                    <p style={{fontSize: "22px"}}>Therefore, the requirement of <b><i>la ilaha illallah</i></b> is to be hostile to everything that is worshipped except Allah, the lord of the worlds.</p>
+
+                    <br />
+                    <hr />
+                    
+                    <p style={{fontSize: "22px", textAlign: "center", marginTop: "30px", color: "red", fontWeight: "bold"}}>وَبَدَا بَيْنَنَا وَبَيْنَكُمُ الْعَدَاوَةُ وَالْبَغْضَاء أَبَدًا حَتَّى تُؤْمِنُوا بِاللَّهِ وَحْدَهُ</p>
+                    <br/>
+                    <p style={{fontSize: "22px"}}><i>The enmity and hatred that has arisen between us and you will last until you believe in Allah alone.”</i></p>
+                    <p style={{fontSize: "20px", textAlign: "center", fontWeight: "bold"}}>(60/Al-Mumtahanah, 4)</p>
+                    
+                    <br />
+                    <br />
+                    
+                    <p style={{fontSize: "22px"}}><b style={{fontSize: "26px"}}>Fourth: </b>It is to takfir those who worship someone other than Allah.</p>
+
+                    <br />
+                    <hr />
+
+                    
+                    <b style={{fontSize: "20px"}}>Almighty Allah says this:</b>
+                    <p style={{fontSize: "22px", textAlign: "center", marginTop: "30px", color: "red", fontWeight: "bold"}}>قُلْ يَا أَيُّهَا الْكَافِرُونَ* لَا أَعْبُدُ مَا تَعْبُدُونَ</p>
+                    <br/>
+                    <p style={{fontSize: "22px"}}><i>Say, ˹O Prophet,˺ “O you disbelievers! I do not worship what you worship,</i></p>
+                    <p style={{fontSize: "20px", textAlign: "center", fontWeight: "bold"}}>(109/Al-Kafirun 1-2)</p>
+
+                    
+                    <p style={{fontSize: "22px"}}>That is, you are unbelievers because you worship those things.</p>
+
+                    
+                    <br />
+                    <br />
+                    
+                    <p style={{fontSize: "22px"}}><b style={{fontSize: "26px"}}>Fifth: </b>Taguta is to stop wanting to be judgment.</p>
+
+                    <br />
+                    <hr />
+
+                    
+                    <b style={{fontSize: "20px"}}>Almighty Allah says this:</b>
+                    <p style={{fontSize: "22px", textAlign: "center", marginTop: "30px", color: "red", fontWeight: "bold"}}>أَلَمْ تَرَ إِلَى الَّذِينَ يَزْعُمُونَ أَنَّهُمْ آمَنُواْ بِمَا أُنزِلَ إِلَيْكَ وَمَا أُنزِلَ مِن قَبْلِكَ يُرِيدُونَ أَن يَتَحَاكَمُواْ إِلَى الطَّاغُوتِ وَقَدْ أُمِرُواْ أَن يَكْفُرُواْ</p>
+                    <br/>
+                    <p style={{fontSize: "22px"}}><i>Have you ˹O Prophet˺ not seen those who claim they believe in what has been revealed to you and what was revealed before you? They seek the judgment of false judges, which they were commanded to reject.</i></p>
+                    <p style={{fontSize: "20px", textAlign: "center", fontWeight: "bold"}}>(4/An-Nisa, 60)</p>
+
+
+                    <p style={{fontSize: "22px"}}>In other words, the beliefs of those who want to judgement in taguta are not real, but only claims.</p>
+
+
+                    <br />
+                    <br />
+                    
+                    <p style={{fontSize: "22px"}}><b style={{fontSize: "26px"}}>Sixth: </b>Not to accept halal and haram setters other than Allah.</p>
+
+                    <br />
+                    <hr />
+
+                    
+                    <b style={{fontSize: "20px"}}>Almighty Allah says this:</b>
+                    <p style={{fontSize: "22px", textAlign: "center", marginTop: "30px", color: "red", fontWeight: "bold"}}>أَمْ لَهُمْ شُرَكَاء شَرَعُوا لَهُم مِّنَ الدِّينِ مَا لَمْ يَأْذَن بِهِ اللَّهُ</p>
+                    <br/>
+                    <p style={{fontSize: "22px"}}><i>Or do they have associate-gods who have ordained for them some halal and haram beliefs, which Allah has not authorized?</i></p>
+                    <p style={{fontSize: "20px", textAlign: "center", fontWeight: "bold"}}>(42/As-Shu'ara, 21)</p>
+
+                    <hr />
+
+                    <p style={{fontSize: "22px", textAlign: "center", marginTop: "30px", color: "red", fontWeight: "bold"}}>اتَّخَذُواْ أَحْبَارَهُمْ وَرُهْبَانَهُمْ أَرْبَابًا مِّن دُونِ اللّهِ</p>
+                    <br/>
+                    <p style={{fontSize: "22px"}}><i>They have taken their rabbis and monks as well as the Messiah, son of Mary, as lords besides Allah,</i></p>
+                    <p style={{fontSize: "20px", textAlign: "center", fontWeight: "bold"}}>(9/At-Tawbah, 31)</p>
+                    
+                    <hr />
+
+                    <p style={{fontSize: "22px", textAlign: "center", marginTop: "30px", color: "red", fontWeight: "bold"}}>حَدَّثَنَا الْحُسَيْنُ بْنُ يَزِيدَ الْكُوفِيُّ، حَدَّثَنَا عَبْدُ السَّلاَمِ بْنُ حَرْبٍ، عَنْ غُطَيْفِ بْنِ أَعْيَنَ، عَنْ مُصْعَبِ بْنِ سَعْدٍ، عَنْ عَدِيِّ بْنِ حَاتِمٍ، قَالَ أَتَيْتُ النَّبِيَّ صلى الله عليه وسلم وَفِي عُنُقِي صَلِيبٌ مِنْ ذَهَبٍ . فَقَالَ " يَا عَدِيُّ اطْرَحْ عَنْكَ هَذَا الْوَثَنَ " . وَسَمِعْتُهُ يَقْرَأُ فِي سُورَةِ بَرَاءَةََ : ( اتَّخَذُوا أَحْبَارَهُمْ وَرُهْبَانَهُمْ أَرْبَابًا مِنْ دُونِ اللَّهِ ) قَالَ " أَمَا إِنَّهُمْ لَمْ يَكُونُوا يَعْبُدُونَهُمْ وَلَكِنَّهُمْ كَانُوا إِذَا أَحَلُّوا لَهُمْ شَيْئًا اسْتَحَلُّوهُ وَإِذَا حَرَّمُوا عَلَيْهِمْ شَيْئًا حَرَّمُوهُ " . قَالَ أَبُو عِيسَى هَذَا حَدِيثٌ غَرِيبٌ لاَ نَعْرِفُهُ إِلاَّ مِنْ حَدِيثِ عَبْدِ السَّلاَمِ بْنِ حَرْبٍ . وَغُطَيْفُ بْنُ أَعْيَنَ لَيْسَ بِمَعْرُوفٍ فِي الْحَدِيثِ </p>
+                    <br/>
+                    <p style={{fontSize: "22px"}}><i>"I came to the Prophet (ﷺ) while I had a cross of gold around my neck. He said: 'O 'Adi! Remove this idol from yourself!' And I heard him reciting from Surah Bara'ah: They took their rabbis and monks as lords besides Allah (9:31). He said: 'As for them, they did not worship them, but when they made something lawful for them, they considered it lawful, and when they made something unlawful for them, they considered it unlawful.'"</i></p>
+                    <p style={{fontSize: "20px", textAlign: "center", fontWeight: "bold"}}>(Thirmidhi, 3095)</p>
+
+
+                    <br />
+                    <br />
+                    
+                    <p style={{fontSize: "22px"}}><b style={{fontSize: "26px"}}>Seventh: </b>To accept all the orders and prohibitions of Sharia.</p>
+                    
+                    <p style={{fontSize: "22px"}}>In other words, it is to accept all the important, non-important, prohibitions, makruh and obligations of the religion as necessary and binding for him. Fulfilling these, on the other hand, will be mentioned in the next bet under the title of the Rights of <b><i>La ilaha Illallah.</i></b></p>
                 </Col>
             </Row>
         </Container>
