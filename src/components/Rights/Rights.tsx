a50f824baa75d5faf32d828c5a5cdc60bbf8a6c8
Mehemmed Qardasov <mehemmed.qardasov2003@gmail.com>
2024-05-02 19:48:51 +0400

    first commit

--- src/components/Rights/Rights.tsx
+++ src/components/Rights/Rights.tsx
@@ -13,11 +13,195 @@ const Rights: React.FC = () => {
                     <h2 className='text-center'>RIGHTS OF LA ILAHE ILLALLAH</h2>
                 </Col>
                 <Col lg={{span: 6, offset:1}} md={{span: 6, offset:1}} sm={12} className='col2'>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit explicabo ea aliquid omnis, quisquam placeat dolorem expedita fugit, iste odit sunt optio aut voluptatem ad vel alias voluptate quia deserunt!
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit explicabo ea aliquid omnis, quisquam placeat dolorem expedita fugit, iste odit sunt optio aut voluptatem ad vel alias voluptate quia deserunt!
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit explicabo ea aliquid omnis, quisquam placeat dolorem expedita fugit, iste odit sunt optio aut voluptatem ad vel alias voluptate quia deserunt!
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit explicabo ea aliquid omnis, quisquam placeat dolorem expedita fugit, iste odit sunt optio aut voluptatem ad vel alias voluptate quia deserunt!
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit explicabo ea aliquid omnis, quisquam placeat dolorem expedita fugit, iste odit sunt optio aut voluptatem ad vel alias voluptate quia deserunt!
+
+                    <p style={{fontSize: "22px", color:"gray"}}>There are some responsibilities that the word <b><i>La ilaha illallah</i></b> imposes on those who accept this word. Accepting that a person has undertaken these responsibilities and seeing them as necessary and binding for him is included in the requirements of <b><i>“La ilahe ilallah”.</i></b> Fulfilling these is included in the rights of <b><i>"La ilaha illallah".</i></b></p>
+
+                    <br />
+                    <hr />
+
+                    <p style={{fontSize: "20px", textAlign: "center", fontWeight: "bold"}}>The Prophet (peace be upon him) says:</p>
+                    <p style={{fontSize: "22px", textAlign: "center", marginTop: "30px", color: "red", fontWeight: "bold"}}> مَنْ قَالَ لاَ إِلَهَ إِلاَّ اللَّهُ وَكَفَرَ بِمَا يُعْبَدُ مِنْ دُونِ اللَّهِ حَرُمَ مَالُهُ وَدَمُهُ وَحِسَابُهُ عَلَى اللَّهِ</p>
+                    <p style={{fontSize: "22px"}}><i>He who professed that there is no god but Allah and made a denial of everything which the people worship beside Allah, his property and blood became inviolable, an their affairs rest with Allah.</i></p>
+                    <p style={{fontSize: "20px", textAlign: "center", fontWeight: "bold"}}>(Buxari; Muslim, 23)</p>
+
+                    <br />
+                    <hr />
+
+                    <p style={{fontSize: "22px"}}>The Prophet (peace be upon him) linked the security of life and property, which is protected by saying <b><i>la ilahe illallah</i></b>, to some of his rights.</p>
+
+                    <p style={{fontSize: "22px"}}>Although each of them is not a condition for the safety of life, all the orders and prohibitions of the Sharia, that is, fulfilling all fard and avoiding all haram, are among the rights of <b><i>la ilaha illallah.</i></b></p>
+
+                    <br />
+                    <hr />
+
+                    <b style={{fontSize: "20px"}}>Almighty Allah says this:</b>
+                    <p style={{fontSize: "22px", textAlign: "center", marginTop: "30px", color: "red", fontWeight: "bold"}}>وَمَآ أُمِرُوٓا۟ إِلَّا لِيَعْبُدُوا۟ ٱللَّهَ مُخْلِصِينَ لَهُ ٱلدِّينَ حُنَفَآءَ وَيُقِيمُوا۟ ٱلصَّلَوٰةَ وَيُؤْتُوا۟ ٱلزَّكَوٰةَ ۚ وَذَٰلِكَ دِينُ ٱلْقَيِّمَةِ</p>
+                    <p style={{fontSize: "22px"}}><i>Even though they were only commanded to worship Allah ˹alone˺ with sincere devotion to Him in all uprightness, establish prayer, and pay alms-tax. That is the upright Way.</i></p>
+                    <p style={{fontSize: "20px", textAlign: "center", fontWeight: "bold"}}>(98/Al-Bayyinah, 5)</p>
+
+                    <br />
+                    <hr />
+
+                    <p style={{fontSize: "22px"}}><b>"To worship Allah ˹alone˺ with sincere", that is the "La ilaha illallah"</b></p>
+
+                    <br />
+
+                    <p style={{fontSize: "22px"}}><b><i>"Performing the prayer and giving the zakat”</i></b> are the greatest rights of <b>la ilaha illallah.</b></p>
+
+                    <br />
+
+                    <p style={{fontSize: "22px"}}>Abandoning prayer, which is the greatest of these rights, removes a person from the religion of Islam according to the correct view.</p>
+                    
+                    <br />
+                    <hr />
+
+                    <p style={{fontSize: "20px", textAlign: "center", fontWeight: "bold"}}>The Prophet (peace be upon him) says:</p>
+                    <p style={{fontSize: "22px", textAlign: "center", marginTop: "30px", color: "red", fontWeight: "bold"}}> إن بين الرجل وبين الشرك والكفر ترك الصلاة</p>
+                    <p style={{fontSize: "22px"}}><i>“Between a person and revile and polytheism is abandonment of prayer.”</i></p>
+                    <p style={{fontSize: "20px", textAlign: "center", fontWeight: "bold"}}>(Muslim)</p>
+
+                    <br />
+                    
+                    <p style={{fontSize: "22px"}}>In other words, there is no barrier between a person who abandons prayer disbelief and polytheism.</p>
+                    
+                    <br />
+
+                    <p style={{fontSize: "22px", textAlign: "center", marginTop: "30px", color: "red", fontWeight: "bold"}}>إن فمن تركها فقد كفر</p>
+                    <p style={{fontSize: "22px"}}><i>“He who abandons her becomes an infidel.”</i></p>
+                    <p style={{fontSize: "20px", textAlign: "center", fontWeight: "bold"}}>(Tirmidhi, Nasai, Ibn Majah)</p>
+
+                    <br />
+
+                    <p style={{fontSize: "22px", textAlign: "center", marginTop: "30px", color: "red", fontWeight: "bold"}}>كان أصحاب محمد صلى الله عليه وسلم لا يرون شيئا من الأعمال تركه كفر غير الصلاة</p>
+                    <p style={{fontSize: "22px"}}><i>"The companions of Prophet sallallahu'alayhiwa sallam would not consider the abandonment of a deed other than prayer as blasphemy.”</i></p>
+                    <p style={{fontSize: "20px", textAlign: "center", fontWeight: "bold"}}>(Tirmidhi)</p>
+
+                    <br />
+                    <hr />
+
+                    <p style={{fontSize: "22px"}}>It is also not known from the Companions who say otherwise. That's why Isaac b. Rahu ye, Ayyub as-Sahtayani and Muhammad b. Imams such as Nasr al-Marwazi narrate this as their consensus.</p>
+
+                    <br />
+
+                    <p style={{fontSize: "22px"}}>While it is the opinion of scholars that whoever abandons prayer will be killed, it is the consensus of the Ahl al-Sunnah in one word that those who resist not doing it even though they are facing death are infidels.</p>
+
+                    <br />
+
+                    <p style={{fontSize: "22px"}}>It is also the consensus of the companions to fight those who do not give zakat, which is the greatest right of <b><i>La ilaha illallah</i></b> after prayer.</p>
+                    
+                    <br />
+                    <hr />
+
+                    <p style={{fontSize: "20px", textAlign: "center", fontWeight: "bold"}}>Abu Bakr as-Siddiq(May Allah be pleased with him) says:</p>
+                    <p style={{fontSize: "22px", textAlign: "center", marginTop: "30px", color: "red", fontWeight: "bold"}}>والله لأقاتلن من فرّق بين الصلاة والزكاة، فإن الزكاة حق المال، والله لو منعوني عقالا كانوا يؤدونه إلى رسول الله صلى الله عليه وسلم لقاتلتهم على منعه</p>
+                    <p style={{fontSize: "22px"}}><i>“By Allah, I will fight with those who Decipher between prayer and zakat. Because zakat is the right of property. If they refuse to give me even a goat they gave to the Messenger of Allah, sallallahu alayhi wa sallam, I will fight them because of this.”</i></p>
+                    <p style={{fontSize: "20px", textAlign: "center", fontWeight: "bold"}}>(Bukhari, Muslim)</p>
+                    
+                    <br />
+                    <hr />
+
+                    <p style={{fontSize: "22px"}}>For this reason, Abu Bakr al-Siddiq (may Allah be pleased with him) fought them, and all the companions agreed with him.</p>
+                    
+                    <br />
+
+                    <p style={{fontSize: "22px"}}>One of the most important rights of <b><i>La ilaha illallah</i></b> is to wage jihad in the way of Allah in order to make the word of Allah, tawhid and <b><i>la ilaha illallah</i></b> the most exalted.</p>
+
+                    <br />
+                    <hr />
+
+                    <b style={{fontSize: "20px"}}>Almighty Allah says this:</b>
+                    <p style={{fontSize: "22px", textAlign: "center", marginTop: "30px", color: "red", fontWeight: "bold"}}>وَقَاتِلُوهُمْ حَتَّى لاَ تَكُونَ فِتْنَةٌ وَيَكُونَ الدِّينُ كُلُّهُ لِلّه</p>
+                    <p style={{fontSize: "22px"}}><i>Fight against them until there is no more persecution—and ˹your˺ devotion will be entirely to Allah.</i></p>
+                    <p style={{fontSize: "20px", textAlign: "center", fontWeight: "bold"}}>(8/Al-Anfal, 39)</p>
+
+                    <br />
+                    <hr />
+
+                    <p style={{fontSize: "22px"}}><b><i>"Until there is no more fitna"</i></b> means until there is no more polytheism. This fully meets the <b><i>"La ilaha"</i></b>, that is, the nefiy.</p>
+
+                    <br />
+
+                    <p style={{fontSize: "22px"}}><b><i>"Until religion becomes completely Allah's"</i></b> means until all worship is performed only to Allah. This fully meets the <b><i>"Illallah"</i></b>, that is, the proof.</p>
+
+                    <br />
+                    <hr />
+
+                    <b style={{fontSize: "20px"}}>Almighty Allah says this:</b>
+                    <p style={{fontSize: "22px", textAlign: "center", marginTop: "30px", color: "red", fontWeight: "bold"}}>قُلْ إِن كَانَ آبَاؤُكُمْ وَأَبْنَآؤُكُمْ وَإِخْوَانُكُمْ وَأَزْوَاجُكُمْ وَعَشِيرَتُكُمْ وَأَمْوَالٌ اقْتَرَفْتُمُوهَا وَتِجَارَةٌ تَخْشَوْنَ كَسَادَهَا وَمَسَاكِنُ تَرْضَوْنَهَا أَحَبَّ إِلَيْكُم مِّنَ اللّهِ وَرَسُولِهِ وَجِهَادٍ فِي سَبِيلِهِ فَتَرَبَّصُواْ حَتَّى يَأْتِيَ اللّهُ بِأَمْرِهِ وَاللّهُ لاَ يَهْدِي الْقَوْمَ الْفَاسِقِينَ</p>
+                    <p style={{fontSize: "22px"}}><i>Say, ˹O Prophet,˺ “If your parents and children and siblings and spouses and extended family and the wealth you have acquired and the trade you fear will decline and the homes you cherish—˹if all these˺ are more beloved to you than Allah and His Messenger and struggling in His Way, then wait until Allah brings about His Will. Allah does not guide the rebellious people.”</i></p>
+                    <p style={{fontSize: "20px", textAlign: "center", fontWeight: "bold"}}>(9/At-Tawbah, 24)</p>
+
+                    <br />
+                    <hr />
+
+                    <p style={{fontSize: "22px"}}>One of the rights of <b><i>La ilaha illallah</i></b> is to emigrate by leaving the bodies and lands of the polytheists whose itikad and deeds have been abandoned.</p>
+
+                    <br />
+                    <hr />
+
+                    <b style={{fontSize: "20px"}}>Almighty Allah says this:</b>
+                    <p style={{fontSize: "22px", textAlign: "center", marginTop: "30px", color: "red", fontWeight: "bold"}}>قَالْوَاْ أَلَمْ تَكُنْ أَرْضُ اللّهِ وَاسِعَةً فَتُهَاجِرُواْ فِيهَا</p>
+                    <p style={{fontSize: "22px"}}><i>The angels will respond, “Was Allah’s earth not spacious enough for you to emigrate?</i></p>
+                    <p style={{fontSize: "20px", textAlign: "center", fontWeight: "bold"}}>(4/An-Nisa, 97)</p>
+
+                    <br />
+
+                    <p style={{fontSize: "22px", textAlign: "center", marginTop: "30px", color: "red", fontWeight: "bold"}}>وَأَعْتَزِلُكُمْ وَمَا تَدْعُونَ مِن دُونِ اللَّهِ وَأَدْعُو رَبِّي عَسَى أَلَّا أَكُونَ بِدُعَاء رَبِّي شَقِيًّا * فَلَمَّا اعْتَزَلَهُمْ وَمَا يَعْبُدُونَ مِن دُونِ اللَّهِ وَهَبْنَا لَهُ إِسْحَقَ وَيَعْقُوبَ وَكُلًّا جَعَلْنَا نَبِيًّا</p>
+                    <p style={{fontSize: "22px"}}><i>As I distance myself from ˹all of˺ you and from whatever you invoke besides Allah, I will ˹continue to˺ call upon my Lord ˹alone˺, trusting that I will never be disappointed in invoking my Lord.” So after he had left them and what they worshipped besides Allah, We granted him Isaac and Jacob, and made each of them a prophet.</i></p>
+                    <p style={{fontSize: "20px", textAlign: "center", fontWeight: "bold"}}>(19/Maryam, 48-49)</p>
+
+                    <br />
+
+                    <p style={{fontSize: "22px", textAlign: "center", marginTop: "30px", color: "red", fontWeight: "bold"}}>يَا عِبَادِيَ الَّذِينَ آمَنُوا إِنَّ أَرْضِي وَاسِعَةٌ فَإِيَّايَ فَاعْبُدُونِ</p>
+                    <p style={{fontSize: "22px"}}><i>O My believing servants! My earth is truly spacious, so worship Me ˹alone˺.</i></p>
+                    <p style={{fontSize: "20px", textAlign: "center", fontWeight: "bold"}}>(29/Al-Ankabut, 56)</p>
+
+                    <br />
+                    <br />
+
+                    <p style={{fontSize: "22px"}}>If you need to leave your homes for the sake of la ilaha illallah, which means only to worship me, do so.</p>
+
+                    <br />
+                    <hr />
+
+                    <p style={{fontSize: "20px", textAlign: "center", fontWeight: "bold"}}>The Prophet (peace be upon him) says:</p>
+                    <p style={{fontSize: "22px", textAlign: "center", marginTop: "30px", color: "red", fontWeight: "bold"}}> أنا بريء من كل مسلم يقيم بين أظهر المشركين</p>
+                    <p style={{fontSize: "22px"}}><i>"I am free from every Muslim who lives among the polytheists."</i></p>
+                    <p style={{fontSize: "20px", textAlign: "center", fontWeight: "bold"}}>(Abu Dawud; Tirmidhi)</p>
+
+                    <br />
+                    <hr />
+
+                    <p style={{fontSize: "22px"}}>One of the rights of <b><i>La ilaha illallah</i></b> is to spend money in the way of Allah for the learning, teaching and dissemination of <b><i>la ilaha illallah</i></b> in order to give thanks for this great blessing and stay constant on it.</p>
+
+                    <br />
+                    <br />
+
+                    <b style={{fontSize: "20px"}}>Almighty Allah says this:</b>
+                    <p style={{fontSize: "22px", textAlign: "center", marginTop: "30px", color: "red", fontWeight: "bold"}}>يَـٰٓأَيُّهَا ٱلَّذِينَ ءَامَنُوٓا۟ إِن تَنصُرُوا۟ ٱللَّهَ يَنصُرْكُمْ وَيُثَبِّتْ أَقْدَامَكُمْ</p>
+                    <p style={{fontSize: "22px"}}><i>O believers! If you stand up for Allah, He will help you and make your steps firm.</i></p>
+                    <p style={{fontSize: "20px", textAlign: "center", fontWeight: "bold"}}>(47/Muhammad, 7)</p>
+
+                    <br />
+                    <br />
+                    <br />
+
+                    <p style={{fontSize: "22px"}}>In other words, if you help the religion of Allah in tawhid, which is the essence and basis of his religion, and <b><i>la ilaha illallah</i></b>, Allah will also help you and keep you stable in his religion and tawhid.</p>
+
+                    <br />
+                    <br />
+
+                    <b style={{fontSize: "20px"}}>Almighty Allah says this:</b>
+                    <p style={{fontSize: "22px", textAlign: "center", marginTop: "30px", color: "red", fontWeight: "bold"}}>هَـٰٓأَنتُمْ هَـٰٓؤُلَآءِ تُدْعَوْنَ لِتُنفِقُوا۟ فِى سَبِيلِ ٱللَّهِ فَمِنكُم مَّن يَبْخَلُ ۖ وَمَن يَبْخَلْ فَإِنَّمَا يَبْخَلُ عَن نَّفْسِهِۦ ۚ وَٱللَّهُ ٱلْغَنِىُّ وَأَنتُمُ ٱلْفُقَرَآءُ ۚ وَإِن تَتَوَلَّوْا۟ يَسْتَبْدِلْ قَوْمًا غَيْرَكُمْ ثُمَّ لَا يَكُونُوٓا۟ أَمْثَـٰلَكُم</p>
+                    <p style={{fontSize: "22px"}}><i>Here you are, being invited to donate ˹a little˺ in the cause of Allah. Still some of you withhold. And whoever does so, it is only to their own loss. For Allah is the Self-Sufficient, whereas you stand in need ˹of Him˺. If you ˹still˺ turn away, He will replace you with another people. And they will not be like you.</i></p>
+                    <p style={{fontSize: "20px", textAlign: "center", fontWeight: "bold"}}>(47/Muhammad, 38)</p>
+
+                    <br />
+                    <br />
+                    <br />
+
+                    <p style={{fontSize: "22px"}}>A person who fulfills the pillars and conditions of <b><i>La ilahe illallah</i></b> and enters Islam, pays the <b><i>prayer</i></b> and <b><i>zakat</i></b>, which are his greatest rights, then gives up his life through  <b><i>jihad</i></b>, his homeland through <b><i>migration</i></b>, his property through <b><i>alms</i></b>, confirms his faith with these great deeds and confirms his belief in <b><i>La ilaha illallah</i></b> he shows that he is faithful.</p>
+
                 </Col>
             </Row>
         </Container>
