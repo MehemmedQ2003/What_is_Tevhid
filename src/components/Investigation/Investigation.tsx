80b6a29b08d7f7a08d4eef38b44bf3126fd85346
Mehemmed Qardasov <mehemmed.qardasov2003@gmail.com>
2024-05-03 21:54:38 +0400

    first commit

--- src/components/Investigation/Investigation.tsx
+++ src/components/Investigation/Investigation.tsx
@@ -13,11 +13,131 @@ const Investigation: React.FC = () => {
                     <h2 className='text-center'>INVESTIGATION OF LA ILAHE ILLALLAH</h2>
                 </Col>
                 <Col lg={{span: 6, offset:1}} md={{span: 6, offset:1}} sm={12} className='col2'>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit explicabo ea aliquid omnis, quisquam placeat dolorem expedita fugit, iste odit sunt optio aut voluptatem ad vel alias voluptate quia deserunt!
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit explicabo ea aliquid omnis, quisquam placeat dolorem expedita fugit, iste odit sunt optio aut voluptatem ad vel alias voluptate quia deserunt!
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit explicabo ea aliquid omnis, quisquam placeat dolorem expedita fugit, iste odit sunt optio aut voluptatem ad vel alias voluptate quia deserunt!
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit explicabo ea aliquid omnis, quisquam placeat dolorem expedita fugit, iste odit sunt optio aut voluptatem ad vel alias voluptate quia deserunt!
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit explicabo ea aliquid omnis, quisquam placeat dolorem expedita fugit, iste odit sunt optio aut voluptatem ad vel alias voluptate quia deserunt!
+
+                    <p style={{fontSize: "26px", color: "gray"}}>The investigation of <b><i>La ilaha illallah</i></b>; to purify one's religion from all kinds of polytheism, bidat and ma'siyat doubts and to be about to move towards monotheism, sunnah and ta'at.</p>
+
+                    <br />
+
+                    <p style={{fontSize: "22px"}}>After the truth of <b><i>La ilaha illallah,</i></b> that is, worshipping only Allah without associating anything with Him, is fulfilled, there are two levels of investigation of this.</p>
+
+                    <br />
+
+                    <p style={{fontSize: "22px"}}>The first level of investigating La ilahe illallah; What is important is investigation.</p>
+
+                    <br />
+
+                    <p style={{fontSize: "22px"}}>The obligatory investigation is accomplished by abandoning four things:</p>
+
+                    <br />
+
+                    <p style={{fontSize: "22px"}}>
+                        <b style={{fontSize: "24px"}}>1</b>- Abandoning petty polytheism. Abandoning the great polytheism, on the other hand, is not related to the investigation of la ilaha illallah, but to its truth.
+
+                        <br />
+                        <br />
+
+                        <b style={{fontSize: "24px"}}>2</b>- To abandon the bid'ats
+
+                        <br />
+                        <br />
+
+                        <b style={{fontSize: "24px"}}>3</b>- To abandon slackness in obligatory matters,
+
+                        <br />
+                        <br />
+
+                        <b style={{fontSize: "24px"}}>4</b>- Do not stop insisting on sins.
+
+                    </p>
+
+                    <br />
+                    <hr />
+                    
+                    <b style={{fontSize: "20px"}}>Almighty Allah says this:</b><p style={{fontSize: "22px", textAlign: "center", marginTop: "30px", color: "red", fontWeight: "bold"}}>إِنَّ الَّذِينَ قَالُوا رَبُّنَا اللَّهُ ثُمَّ اسْتَقَامُوا فَلَا خَوْفٌ عَلَيْهِمْ وَلَا هُمْ يَحْزَنُونَ* أُوْلَئِكَ أَصْحَابُ الْجَنَّةِ خَالِدِينَ فِيهَا جَزَاء بِمَا كَانُوا يَعْمَلُونَ</p>
+                    <br/>
+                    <p style={{fontSize: "22px"}}><i>Surely those who say, “Our Lord is Allah,” and then remain steadfast—there will be no fear for them, nor will they grieve. It is they who will be the residents of Paradise, staying there forever, as a reward for what they used to do.</i></p>
+                    <p style={{fontSize: "20px", textAlign: "center", fontWeight: "bold"}}>(46/Al-Ahqaf, 13-14)</p>
+
+                    <br />
+                    <hr />
+
+                    <p style={{fontSize: "22px"}}>To say, "Our Lord is Allah" is to realize the essence of la ilaha illallah, that is, to worship only Allah without associating anything with Him.</p>
+
+                    <br />
+
+                    <p style={{fontSize: "22px"}}>"Being on the right path" means fulfilling the obligatory acts and abandoning the harams.</p>
+
+                    <br />
+
+                    <p style={{fontSize: "22px"}}>After fulfilling the essence of Tawhid, the person who abandons minor polytheism, innovations and insisting on sins will have fulfilled the obligatory investigation of <b><i>la ilahe illallah.</i></b></p>
+
+                    <br />
+
+                    <p style={{fontSize: "22px"}}>The second order of investigating La ilaha illallah is the investigation that is mustahab.</p>
+
+                    <br />
+
+                    <p style={{fontSize: "22px"}}>The investigation that is mustahab also takes place by abandoning these four things.</p>
+
+                    <br />
+
+                    <p style={{fontSize: "22px"}}>The investigation that is mustahab also takes place by abandoning these four things.</p>
+
+                    <br />
+
+                    <p style={{fontSize: "22px"}}>
+                        <b style={{fontSize: "24px"}}>1</b>- To leave makruhs
+
+                        <br />
+                        <br />
+
+                        <b style={{fontSize: "24px"}}>2</b>- Abandoning questionable things
+
+                        <br />
+                        <br />
+
+                        <b style={{fontSize: "24px"}}>3</b>- To abandon unnecessary permissible things
+
+                        <br />
+                        <br />
+
+                        <b style={{fontSize: "24px"}}>4</b>- Abandoning asking from anyone other than Allah
+
+                    </p>
+
+                    <br />
+
+                    <p style={{fontSize: "22px"}}>If the person who fulfills the essence of Tawhid, abandons minor polytheism, innovations and insisting on sins, and carries out the obligatory investigation of La ilahe illallah, also abandons the makruhs, doubtful things, superfluous permissibles and asking from anyone other than Allah, the person who fulfills the obligation of la ilahe illallah. It also fulfills the recommended investigation.</p>
+
+                    <br />
+
+                    <p style={{fontSize: "22px"}}>The truth of this is that the soul turns only to Allah Almighty with love, fear and hope, and worships Him alone with sincerity, reverence and humiliation. It is that the heart does not trust and tell on anyone other than Allah, and does not cherish fear and hope for anyone other than Him. It means that he does not have any desires or expectations from anyone other than Him, and that he does not desire anyone other than Him.</p>
+
+                    <br />
+                    <hr />
+
+                    <p style={{fontSize: "20px", textAlign: "center", fontWeight: "bold"}}>The Prophet (peace be upon him) also says:</p>
+                    <p style={{fontSize: "22px", textAlign: "center", marginTop: "30px", color: "red", fontWeight: "bold"}}>من أحب لله وأبغض لله وأعطى لله ومنع لله فقد استكمل الإيمان</p>
+                    <p style={{fontSize: "22px"}}><i>"Whoever loves for Allah, hates for Allah, gives for Allah and withholds for Allah will have completed faith."</i></p>
+                    <p style={{fontSize: "20px", textAlign: "center", fontWeight: "bold"}}>(Abi Dawud, Tirmidhi)</p>
+
+                    <br />
+                    <hr />
+
+                    <p style={{fontSize: "22px"}}>If he speaks, he speaks for Him, if he is silent, he is silent for Him, if he moves, he moves for Him.</p>
+
+                    <br />
+
+                    <p style={{fontSize: "22px"}}>He sees the greatest happiness and happiness in remembering Him, and the greatest misery in remaining unaware of Him.</p>
+
+                    <br />
+
+                    <p style={{fontSize: "24px", fontWeight: "bolder"}}>Imam Muhammad bin Sulayman Jul-Temimi Kitabu't-Tawhid also says the following:</p>
+
+                    <br />
+
+                    <p style={{fontSize: "22px", fontStyle: "italic"}}>He who investigates Tawhid will enter heaven without any calculation.</p>
+
                 </Col>
             </Row>
         </Container>
